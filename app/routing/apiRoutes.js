3c86bf7300a29e11662cc585b5fdd01dc3bcd067
ryan-judy <rj113909@gmail.com>
2017-07-26 20:44:03 -0400

    everythings good

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,75 +9,43 @@ let app = function(app){
 
 	 	res.json(friends);
 
-	 	var newFriend = {
-
-	        name: "Ryan",
-	        photo: "http:",
-	        scores:[  
-				5,
-			    1,
-			    4,
-			    4,
-			    5,
-			    1,
-			    2,
-			    5,
-			    4,
-			    1
-	        ]
-	      };
-
-
-	 	for (var i = 0; i < friends.length; i++) {
-			var scoreArray = friends[i].scores;
-					var friendArray = [];
-
-
-			for (var x = 0; x < scoreArray.length; x++) {
-				var friendScore = scoreArray[x];
-				//console.log(friendScore)
-
-
-				for (var y = 0; y < friendScore.length; y++) {
-					var result = newFriend.scores[0] - friendScore[y];
-					parseInt(result);
-
-					friendArray.push(result)
-
-				}
-
-				//console.log(friendArray)
-					var sum = 0;
-
-					for(var z=0; z < friendArray.length; z++){
-
-					    sum += parseInt(friendArray[z]);
-
-					}
-
-				};
-					parseInt(sum)
-					totalDifference.push(
-						friends[i]["total"] = parseInt(sum));
-					//console.log(totalDifference)
-					var lowest = Math.min.apply(null, totalDifference);
-					if (lowest === friends[i].total) {
-						console.log(friends[i].name)
-					}
-					
-			};	
-
-	});  
+  	});
 
 	app.post('/api/friends', function(req, res){
 
 	 	var newfriend = req.body;
 
+		var userResponse = newfriend.scores;
+
+		var matchName = "";
+		var matchImage = "";
+		var totalDifference = 100;
+
+		for (var i = 0; i < friends.length; i++) {
+
+			var friendDiff = 0;
+
+			for (var j = 0; j < userResponse.length; j++) {
+				friendDiff += Math.abs(friends[i].scores[j] - userResponse[j]);
+
+			}
+
+			if (friendDiff < totalDifference) {
+
+				totalDifference = friendDiff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+				
+			}
+		}
 
 		friends.push(newfriend);
+
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 
 	});  	
 
 }
+
 
 module.exports = app
